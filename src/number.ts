b56224a9d4cfd7677f493782c7782de581d05678
yubo9807 <yubo9807@gmail.com>
2023-03-15 14:29:10 +0800

    feat: isOdd and isEven

--- src/number.ts
+++ src/number.ts
@@ -1,4 +1,39 @@
 /**
+ * 判断数字是否是一个基数
+ * @param num 
+ * @returns 
+ */
+export function isOdd(num: number) {
+  // -3 / 2 = -1.5
+  return num / 2 === 1 || num % 2 === -1;
+}
+
+/**
+ * 判断一个数是否为偶数
+ * @param num
+ * @returns
+ */
+export function isEven(num: number) {
+  const c = num / 2;
+  return c / Math.floor(c) === 1 || c === 0;
+}
+
+/**
+ * 求模
+ * @param x 
+ * @param y 
+ * @returns 
+ */
+export function mod(x: number, y: number) {
+  // 在取余运算中，余数的符号和x（被除数）相同
+  // 在取余运算中，余数的符号和x（除数）相同
+  x = Math.abs(x);
+  const c = Math.floor(x / Math.abs(y));
+  const m = x - c * Math.abs(y);
+  return y >= 0 ? m : -m;
+}
+
+/**
  * 生成随机数
  * @param max 最大值（取不到，只可取正整数）
  * @param min 最小值
@@ -155,4 +190,30 @@ function toHundreds(num: number) {
 export function isPowerOf2(x: number) {
   if (x <= 0) return false;
   return (x & (x - 1)) === 0;
+}
+
+/**
+ * 计算元素中的一个点的角度（坐标轴中心即元素中心）
+ * @param width  元素宽度（px） 
+ * @param height 元素高度（px）
+ * @param x      offsetX
+ * @param y      offsetY
+ * @returns
+ */
+export function elPointDegree(width: number, height: number, x: number, y: number) {
+  return pointDegree(x - width / 2, y - height / 2);
+}
+
+/**
+ * 计算一个点的角度（坐标轴中心：(0,0)）
+ * @param x 基于象限的 x 坐标
+ * @param y 基于象限的 y 坐标
+ * @returns
+ */
+export function pointDegree(x: number, y: number) {
+  if (y <= 0) {
+    return Math.abs(Math.atan2(y, x) * 180 / Math.PI);
+  } else {
+    return Math.abs(Math.atan2(y, -x) * 180 / Math.PI) + 180;
+  }
 }
