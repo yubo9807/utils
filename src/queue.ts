15684544b5b4c02d59de7a762d205d8f74e0d9e5
yubo9807 <yubo9807@gmail.com>
2023-03-15 14:28:06 +0800

    feat: queue

---
+++ src/queue.ts
@@ -0,0 +1,243 @@
+import { PromiseFn } from "./type";
+
+type TaskItem = {
+  task:    PromiseFn
+  resolve: Function
+  reject:  Function
+}
+
+
+
+export class ParallelTaskScheduling {
+  _parallelCount: number  // 并行执行任务数
+  _runingCount = 0;       // 执行任务计数
+  tasks: TaskItem[] = [];
+
+  /**
+   * 并行任务调度
+   * @param parallelCount 并行执行任务最大数
+   */
+  constructor(parallelCount = 5) {
+    this._parallelCount = parallelCount;
+  }
+
+  /**
+   * 添加一个任务
+   * @param task 
+   * @returns 
+   */
+  add(task: PromiseFn) {
+    const promise = new Promise((resolve, reject) => {
+      this.tasks.push({ task, resolve, reject });
+    })
+    this._run();
+    return promise;
+  }
+
+  /**
+   * 并行运行任务
+   */
+  _run() {
+    while (this.tasks.length > this._runingCount && this._runingCount < this._parallelCount) {
+      const { task, resolve, reject } = this.tasks[this._runingCount];
+      this._runingCount ++;
+      task()
+        .then(res => resolve(res))
+        .catch(err => reject(err))
+        .finally(() => {
+          this._continue();
+        });
+    }
+  }
+
+  /**
+   * 继续执行下个任务
+   */
+  _continue() {
+    this._parallelCount ++;  // 将并行执行数调大，保证能再次进入循环
+    this._run();             // 执行完递归执行剩余任务
+  }
+}
+
+
+
+export class ParallelTaskSchedulings extends ParallelTaskScheduling {
+  _parallelCountList: number[];
+  _parallelListIndex = 0;  // 并行执行任务数索引
+  _executeCount = 0;       // 任务执行计数
+  _isAwait: boolean;
+  #originRun = null;
+
+  /**
+   * 并行任务调度（按指定的并行执行数执行）
+   * @param parallelCountList 并行执行队列 LIST
+   * @param isAwait true: 队列清空后再执行剩余任务  false: 队列有空间即执行剩余任务
+   */
+  constructor(parallelCountList: number[], isAwait = false) {
+    parallelCountList.forEach((val, i, self) => {
+      if (i === 0) return;
+      self[i] = val + self[i - 1];
+    })
+
+    super();
+    this._parallelCountList = parallelCountList;
+    this._isAwait = isAwait;
+    this.#originRun = this._run;
+    this._run = () => {
+      this._parallelCount = this._parallelCountList[this._parallelListIndex];
+      this.#originRun();
+    }
+  }
+
+  /**
+   * 继续执行下个任务
+   */
+  _continue() {
+    this._executeCount ++;
+    if (this._isAwait) {  // 等待队列执行结束完
+      if (this._executeCount === this._parallelCount) {
+        this._parallelListIndex ++;
+        this._run();
+        return;
+      }
+    } else {  // 不等了，继续往下执行
+      if (this._runingCount === this._parallelCount) {
+        this._parallelListIndex ++;
+        this._run();
+      }
+    }
+    this._parallelCount ++;
+    this._run();
+  }
+}
+
+
+
+export class SerialTaskScheduling extends ParallelTaskScheduling {
+  #isContinue: boolean
+
+  /**
+   * 串行任务调度
+   * @param isContinue 在失败后继续执行下个任务
+   */
+  constructor(isContinue = true) {
+    super();
+    this._parallelCount = 1;
+    this.#isContinue = isContinue;
+  }
+
+  _run() {
+    while (this.tasks.length > this._runingCount && this._runingCount < this._parallelCount) {
+      const { task, resolve, reject } = this.tasks[this._runingCount];
+      this._runingCount ++;
+      task()
+        .then(res => {
+          resolve(res);
+          this._continue();
+        })
+        .catch(err => {
+          reject(err);
+          this.#isContinue && this._continue();
+        })
+    }
+  }
+}
+
+
+
+export class PausingSerialTasks {
+
+  readonly START_FAILURE = Symbol('startFailure');  // 任务队列被提前回收，启动失败
+  readonly ILLEGAL_CALL  = Symbol('illegalCall');   // 非法调用，任务队列正在执行中
+  readonly NOT_TASK      = Symbol('notTask');       // 没有任务可执行
+
+  #isRuning  = false;  // 队列正在执行中
+  #isExecute = false;  // 是否执行队列
+  #tasks     = null;   // 任务队列
+  #i         = 0;      // 执行队列下标
+
+  /**
+   * 可暂停的串行任务队列
+   * 如果其中一个任务失败，返回失败结果，后续任务都不会执行
+   * 在执行过程中或执行前可追加任务
+   * 若在整个执行结束后追加了任务，请重新执行 start
+   * @param tasks ...args 任务队列
+   */
+  constructor(...tasks: Array<PromiseFn>) {
+    this.#tasks = tasks;
+  }
+
+  /**
+   * 开始/继续 执行任务
+   * @returns 暂停～开始～暂停/结束 的每项任务的返回结果
+   */
+  start = async () => {
+    if (this.#isRuning) {
+      console.warn('非法调用！任务队列正在执行中，无法重新执行');
+      return Promise.reject(this.ILLEGAL_CALL);
+    }
+
+    // 超出执行队列长度
+    if (this.#i > this.#tasks.length - 1) {
+      console.warn('没有任务可以继续执行');
+      return Promise.resolve(this.NOT_TASK);
+    }
+
+    this.#isRuning  = true;
+    this.#isExecute = true;
+    const promise   = await this.#execute();
+    this.#isRuning  = false;
+    return promise;
+  }
+
+  /**
+   * 递归执行队列
+   * @param results 收集返回值
+   * @returns 
+   */
+  #execute = async (results = []) => {
+    if (!this.#isExecute) return;
+    const len = this.#tasks.length;
+    if (this.#i === len - 1) this.#isExecute = false;  // 执行到最后一个任务，暂停任务
+
+    const result = this.#tasks[this.#i]();
+    this.#i ++;
+    return await result.then(async res => {
+      return await handleResult.call(this, res);
+    }).catch(err => {
+      this.#isExecute = false;
+      return Promise.reject(err);
+    });
+
+    async function handleResult(res) {
+      results.push(res);
+      if (this.#tasks.length > len) {
+        this.#isExecute = true;  // 发现中途有队列推送，继续递归执行
+        return await this.#execute(results);
+      }
+      if (!this.#isExecute) {
+        return Promise.resolve(results);
+      } else {
+        return await this.#execute(results);
+      }
+    }
+  }
+
+  /**
+   * 暂停 任务执行
+   * @returns 当前暂停的任务下标
+   */
+  pause = () => {
+    this.#isExecute = false;
+    return this.#i - 1;
+  }
+
+  /**
+   * 追加任务
+   * @param fn 
+   */
+  add = (fn: PromiseFn) => {
+    this.#tasks && this.#tasks.push(fn);
+  }
+
+}
