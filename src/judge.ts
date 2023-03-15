a802c115c49bc71e5d599b782ccff934982b6785
yubo9807 <yubo9807@gmail.com>
2023-03-15 14:29:37 +0800

    feat: isPromiseLike, isPromiseLike

--- src/judge.ts
+++ src/judge.ts
@@ -2,12 +2,11 @@
 export type Type = 'string'    | 'number'  | 'boolean' |
                    'symbol'    | 'bigint'  |
                    'undefined' | 'null'    |
+                   'regexp'    | 'date'    |
                    'array'     | 'object'  |
                    'function'  | 'promise' |
                    'set'       | 'map'     |
-                   'weakset'   | 'weakmap' | 'weakref' |
-                   'regexp'
-
+                   'weakset'   | 'weakmap' | 'weakref'
 /**
  * 判断数据是什么类型
  * @param o 
@@ -33,4 +32,33 @@ export function isAsync(func: Function): boolean {
  */
 export function isPromise(result: any): boolean {
   return typeof result === 'object' && result[Symbol.toStringTag] === 'Promise';
+}
+
+/**
+ * 是否属于一个 PromiseLike
+ * @param value 
+ * @returns 
+ */
+export function isPromiseLike(value: any) {
+  return (typeof value === 'object' || typeof value === 'function') && typeof value.then === 'function';
+}
+
+/**
+ * 判断两个值是否相等
+ * @param val1 
+ * @param val2 
+ * @returns 
+ */
+export function isEquals(val1: any, val2: any) {
+  if (typeof val1 === 'object' && typeof val2 === 'object') {
+    const keys1 = Object.keys(val1), keys2 = Object.keys(val2);
+    if (keys1.length !== keys2.length) return false;
+    for (const key of keys1) {
+      if (!keys2.includes(key)) return false;
+      return isEquals(val1[key], val2[key]);
+    }
+    return true;
+  } else {
+    return val1 === val2;
+  }
 }
